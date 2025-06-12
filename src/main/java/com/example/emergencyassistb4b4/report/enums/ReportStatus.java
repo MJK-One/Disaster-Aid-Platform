b00de72a286a4eefbe44a480eb7fa39cb84f3677
Prototype-Km <kgm970910@gmail.com>
2025-06-12 14:26:27 +0900

    feat : report 생성시 PENDING, updateStatus, ReportResponse엔티티 수정

--- src/main/java/com/example/emergencyassistb4b4/report/enums/ReportStatus.java
+++ src/main/java/com/example/emergencyassistb4b4/report/enums/ReportStatus.java
@@ -1,7 +1,7 @@
 package com.example.emergencyassistb4b4.report.enums;
 
 public enum ReportStatus {
-    PENDING,
-    RECEIVED,
-    CLOSED
+    PENDING,   //대기
+    RECEIVED,  //확인
+    CLOSED     //종료
 }
