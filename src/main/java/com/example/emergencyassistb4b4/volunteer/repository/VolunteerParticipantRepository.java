91c5bc75ebf2577497900724282dfe52b4701a41
MJK-One <hanelejss012@naver.com>
2025-06-12 09:48:18 +0900

    chore: add/update repository layer

---
+++ src/main/java/com/example/emergencyassistb4b4/volunteer/repository/VolunteerParticipantRepository.java
@@ -0,0 +1,7 @@
+package com.example.emergencyassistb4b4.volunteer.repository;
+
+import com.example.emergencyassistb4b4.volunteer.domain.VolunteerParticipant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VolunteerParticipantRepository extends JpaRepository<VolunteerParticipant, Long> {
+}
---
+++ src/main/java/com/example/emergencyassistb4b4/volunteer/repository/VolunteerTeamRepository.java
@@ -0,0 +1,11 @@
+package com.example.emergencyassistb4b4.volunteer.repository;
+
+import com.example.emergencyassistb4b4.volunteer.domain.VolunteerTeam;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VolunteerTeamRepository extends JpaRepository<VolunteerTeam, Long> {
+    Optional<VolunteerTeam> findByPost_IdAndTeamNumber(Long postId, int teamNumber);
+
+}
