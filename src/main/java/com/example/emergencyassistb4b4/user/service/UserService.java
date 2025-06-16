1439957d53493fb45030e229b1386c95a0bdd48b
mini-project-todo-list <leehanna0720@gmail.com>
2025-06-16 10:35:52 +0900

    fix: 아래 커밋이랑 똑같음(커밋에서 파일 누락됨)

--- src/main/java/com/example/emergencyassistb4b4/user/service/UserService.java
+++ src/main/java/com/example/emergencyassistb4b4/user/service/UserService.java
@@ -2,11 +2,16 @@ package com.example.emergencyassistb4b4.user.service;
 
 import com.example.emergencyassistb4b4.global.exception.ApiException;
 import com.example.emergencyassistb4b4.global.status.ErrorStatus;
+import com.example.emergencyassistb4b4.report.domain.Report;
+import com.example.emergencyassistb4b4.report.repository.ReportRepository;
+import com.example.emergencyassistb4b4.report.repository.ReportResponseRepository;
 import com.example.emergencyassistb4b4.user.domain.User;
+import com.example.emergencyassistb4b4.user.dto.UserRequestDto;
 import com.example.emergencyassistb4b4.user.dto.UserResponseDto;
 import com.example.emergencyassistb4b4.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,11 +19,31 @@ public class UserService {
 
 
     private final UserRepository userRepository;
+    private final ReportRepository reportRepository;
+    private final ReportResponseRepository reportResponseRepository;
 
-    public UserResponseDto getMyInfo( String email) {
-        User user = userRepository.findByEmail(email)
+    public UserResponseDto getMyInfo(UserRequestDto userRequestDto) {
+        User user = userRepository.findByEmail(userRequestDto.getEmail())
                 .orElseThrow( () -> new ApiException(ErrorStatus.USER_NOT_FOUND));
         return UserResponseDto.from(user);
+    }
+
+    @Transactional(readOnly = true)
+    public User getReporterInfo(Long reportId, User responder) {
+
+        // 신고 조회
+        Report report = reportRepository.findById(reportId)
+                .orElseThrow(() -> new ApiException(ErrorStatus.REPORT_NOT_FOUND));
+
+        // 권한 확인 (해당 신고가 내가 담당하는 신고인지 확인)
+        boolean isMyReport = reportResponseRepository.existsByReportAndResponder(report, responder);
+
+        if (!isMyReport) {
+            throw new ApiException(ErrorStatus.CUSTOM_ERROR_STATUS);
+        }
+
+        // 신고자 반환
+        return report.getReporter();
     }
 
 
