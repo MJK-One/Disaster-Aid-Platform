60d4f3baf49f45723d7c95322833132be44c6f3f
khg9900 <whale4100@gmail.com>
2025-06-24 10:05:26 +0900

    fix

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -3,26 +3,38 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../api/auth/screens/loginScreen';
 import SignUpScreen from '../api/auth/screens/singupScreen';
 import WelcomeScreen from '../WelcomeScreen';
-// import VolunteerPostListScreen from '../api/volunteer/screens/VolunteerPostListScreen';
-// import VolunteerPostCreateScreen from '../api/volunteer/screens/VolunteerPostCreateScreen';
+
+import VolunteerPostListScreen from '../api/volunteer/screens/VolunteerPostListScreen';
+import VolunteerPostCreateScreen from '../api/volunteer/screens/VolunteerPostCreateScreen';
 import MyActivitiesScreen from '../api/report/screens/MyActivitiesScreen';
 import ReportListPage from '../api/report/screens/ReportListPage'
 import AlertScreen from '../api/alert/screens/AlertScreen';
+import Layout from '../components/Layout';
 
 const Stack = createNativeStackNavigator();
 
+const withLayout = (Component: React.ComponentType) => {
+  return (props: any) => (
+    <Layout>
+      <Component {...props} />
+    </Layout>
+  );
+};
+
 const RootNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Welcome">
-      <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }} />
+    <Stack.Navigator initialRouteName="Login">
+      {/* ❌ Layout 미적용 */}
       <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
       <Stack.Screen name="SignUp" component={SignUpScreen} options={{ headerShown: false }} />
+
+      {/* ✅ Layout 적용 */}
+      <Stack.Screen name="Welcome" component={withLayout(WelcomeScreen)} options={{ headerShown: false }} />
+      <Stack.Screen name="VolunteerPosts" component={withLayout(VolunteerPostListScreen)} options={{ headerShown: false }} />
+      <Stack.Screen name="PostCreate" component={withLayout(VolunteerPostCreateScreen)} options={{ headerShown: false }} />
       <Stack.Screen name="MyActivities" component={MyActivitiesScreen}options={{ title: '나의 활동' }} />
       <Stack.Screen name="ReportList" component={ReportListPage} options={{ title: '내 신고 목록' }} />
       <Stack.Screen name="Alert" component={AlertScreen} options={{ headerShown: false }} /> 
-
-    {/* <Stack.Screen name="Attendance" component={AttendanceScreen} options={{ title: '출석' }} />
-    <Stack.Screen name="Participants" component={ParticipantsScreen} options={{ title: '참가자' }}/> */}
     </Stack.Navigator>
   );
 };
